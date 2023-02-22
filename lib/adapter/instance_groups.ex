763dd04930e7f1071c778255ed7351515f7d5037
Luis 🦎 <luis.6496@hotmail.com>
2023-02-22 11:16:20 -0600

    Refactor to instance group module

--- lib/adapter/instance_groups.ex
+++ lib/adapter/instance_groups.ex
@@ -1,9 +1,6 @@
 defmodule Cluster.Strategy.Adapter.InstanceGroups do
   @moduledoc """
-  Find all the instances available in a  managed instance group
-  A managed instance group is a group of homogeneous instances based on an instance template.
-
-  https://cloud.google.com/compute/docs/instance-groups/creating-groups-of-managed-instances
+  Find all the instances available in a project
 
   """
   @behaviour Cluster.Strategy.Adapter
@@ -19,7 +16,7 @@ defmodule Cluster.Strategy.Adapter.InstanceGroups do
   require Logger
 
   @doc """
-  return a list of compute instances, available in the specified topology
+  return a list of compute instances, available in the specified project
   """
   def get_nodes(release_name, config) do
     _ =
@@ -33,42 +30,10 @@ defmodule Cluster.Strategy.Adapter.InstanceGroups do
          release_name,
          project
        ) do
-    conn = get_access_token() |> Connection.new()
-
-    instances =
-      Instances.compute_instances_aggregated_list(
-        conn,
-        project
-      )
-
-    case instances do
-      {:ok, %InstanceAggregatedList{items: items}} ->
-        nodes =
-          items
-          |> Enum.filter(fn
-            {_zone, %{warning: nil}} -> true
-            _ -> false
-          end)
-          |> Enum.map(fn {_zone, %InstancesScopedList{instances: instances}} ->
-            instances
-            |> Enum.filter(fn
-              %Instance{status: "RUNNING"} -> true
-              _ -> false
-            end)
-            |> Enum.map(fn %Instance{name: name, zone: zone} ->
-              %{"zones" => zone} = Regex.named_captures(~r/.*\/(?<zones>.*)$/, zone)
-
-              :"#{release_name}@#{name}.#{zone}.c.#{project}.internal"
-            end)
-          end)
-          |> List.flatten()
-
-        {:ok, nodes}
-
-      e ->
-        Logger.error(inspect(e))
-        {:error, e}
-    end
+    get_access_token()
+    |> Connection.new()
+    |> Instances.compute_instances_aggregated_list(project)
+    |> iterate_instance_items(release_name, project)
   end
 
   defp get_access_token() do
@@ -76,4 +41,43 @@ defmodule Cluster.Strategy.Adapter.InstanceGroups do
 
     response.token
   end
+
+  defp iterate_instance_items(
+         {:ok, %InstanceAggregatedList{items: items}},
+         release_name,
+         project
+       ) do
+    nodes =
+      items
+      |> Enum.filter(&filter_by_warnings/1)
+      |> Enum.map(&build_dns_name(&1, release_name, project))
+      |> List.flatten()
+
+    {:ok, nodes}
+  end
+
+  defp iterate_instance_items(error, _release_name, _project) do
+    Logger.error(inspect(error))
+    {:error, error}
+  end
+
+  defp build_dns_name(
+         {zone, %InstancesScopedList{instances: instances}},
+         release_name,
+         project
+       ) do
+    %{"regions" => zone} = Regex.named_captures(~r/.*\/(?<regions>.*)$/, zone)
+
+    instances
+    |> Enum.filter(&filter_by_running_instances/1)
+    |> Enum.map(fn %Instance{name: name} ->
+      :"#{release_name}@#{name}.#{zone}.c.#{project}.internal"
+    end)
+  end
+
+  defp filter_by_warnings({_zone, %{warning: nil}}), do: true
+  defp filter_by_warnings(_), do: false
+
+  defp filter_by_running_instances(%Instance{status: "RUNNING"}), do: true
+  defp filter_by_running_instances(_), do: false
 end
